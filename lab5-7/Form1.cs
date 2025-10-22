a4222c1a708c8441cf1d5bbc7262aa7d9b475535
VisoredApollo <vbeketov2006@gmail.com>
2025-10-22 19:31:41 +0300

    Создан оптимальный поиск

--- lab5-7/Form1.cs
+++ lab5-7/Form1.cs
@@ -44,7 +44,34 @@ namespace lab5_7
         {
             int Key = Decimal.ToInt32(KeyUD.Value);
 
-            
+            // Оптимальный поиск
+            {
+                int Index = -1;
+
+                int LeftBoder;
+                int RightBoder;
+                int i;
+
+                int StartTime = Environment.TickCount;
+                for (int cycle = 0; cycle < CYCLES; cycle++)
+                {
+                    LeftBoder = 0;
+                    RightBoder = N - 1;
+                    while (RightBoder > LeftBoder)
+                    {
+                        i = (LeftBoder + RightBoder) / 2;
+                        if (Key <= Array[i]) RightBoder = i;
+                        else LeftBoder = i + 1;
+                    }
+                    if (Key == Array[RightBoder]) Index = RightBoder;
+                }
+                int ResultTime = Environment.TickCount - StartTime;
+
+                if (Index != -1) OptimalIndex.Text = Index.ToString();
+                else OptimalIndex.Text = "Не найдено";
+
+                OptimalTime.Text = ResultTime.ToString();
+            }
         }
     }
 }
